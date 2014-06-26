9a0b9c298d051eb7d9a85d5f8879b535b71e6791
Erik Shandrowski <erik843@gmail.com>
2014-06-26 14:15:23 -0600

    Part 1 completed

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).on('ready', function() {
   
+  $('.textbox').on('click', function() {
+  	$(this).toggleClass("is-hidden");
+  	$(this).next().toggleClass("is-hidden");
+  	$(this).next().off('click');
+  })
+
+  $('.save-button').on('click', function() {
+  	var movieInput = $(this).siblings('input').val();
+  	console.log(movieInput);
+  	$(this).parent().toggleClass("is-hidden");
+  	$(this).parent().prev().toggleClass("is-hidden");
+  	$(this).parent().prev().html("<p>" + movieInput + "</p>");
+  })
+
 });
