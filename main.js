687e509032674f7f7230477336617f50a9f167ba
Erik Shandrowski <erik843@gmail.com>
2014-06-26 17:25:32 -0600

    Exercise finished

--- main.js
+++ main.js
@@ -7,11 +7,26 @@ $(document).on('ready', function() {
   })
 
   $('.save-button').on('click', function() {
-  	var movieInput = $(this).siblings('input').val();
-  	console.log(movieInput);
+  	var movieInput = $(this).prev('input').val();
   	$(this).parent().toggleClass("is-hidden");
   	$(this).parent().prev().toggleClass("is-hidden");
   	$(this).parent().prev().html("<p>" + movieInput + "</p>");
+  	$(this).parent().prev().css('color', 'blue');
   })
+// This selects whole document and any element that has 
+// a 'contentEditable' attribute will be editable
+/*  	$(document).on('click', function(e) {
+  		$(e.target).attr('contentEditable', true);
+  	});*/
 
+// This uses x-editable 
+  	$.fn.editable.defaults.mode = 'inline';
+  	$('#x-editable').editable( {
+  			type: 'textarea',
+  			pk: 1,
+  			url: '',
+  			title: 'Enter Comments',
+  			rows: 10,
+  			inputclass: 'textbox',
+  		});
 });
